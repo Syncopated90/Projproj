e8a46b09db07883bdeaa2bd0064bc7f8b08c1907
Syncopated90 <ehne@kth.se>
2023-04-24 15:21:33 +0200

    can push some data to firebase

--- smartbrew/src/firebaseModel.js
+++ smartbrew/src/firebaseModel.js
@@ -1,7 +1,11 @@
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set} from "firebase/database";
+import {initializeApp} from 'firebase/app'
+import {app, database, firebaseConfig} from './firebaseConfig';
 
 export default function writeUserData(userId, name, email) {
-  const db = getDatabase();
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase(app);
+  
   set(ref(db, 'users/' + userId), {
     username: name,
     email: email,
