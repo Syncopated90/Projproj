e5981fa53412afdd6076c89c886b7a7c432f3dd4
Serdar-b <serdar-bulun@outlook.com>
2023-04-24 21:53:52 +0200

    Created a water level view with a water level state and buttons to increment/decrement the state

--- smartbrew/src/presenters/waterLevelPresenter.js
+++ smartbrew/src/presenters/waterLevelPresenter.js
@@ -1,8 +1,21 @@
 import WaterLevelView from "../views/waterLevelView";
 import React, {useState} from 'react'
-import writeUserData from '../firebaseModel';
 
-export default
-function WaterLevel(){
-    return(<WaterLevelView/>);
+export default function WaterLevel(){
+    const [waterLevelState, setWaterLevelState] = useState(0);
+
+    const clickedOnIncrementHandler = () =>{
+        setWaterLevelState(waterLevelState + 1)
+    }
+
+    const clickedOnDecrementHandler = () =>{
+        setWaterLevelState(waterLevelState - 1)
+    }
+
+    return (
+    <WaterLevelView 
+    onClickIncrement = {clickedOnIncrementHandler} 
+    onClickDecrement  = {clickedOnDecrementHandler} 
+    value = {waterLevelState}/>
+    );
 }
--- smartbrew/src/views/waterLevelView.js
+++ smartbrew/src/views/waterLevelView.js
@@ -1,6 +1,10 @@
 function WaterLevelView(props){
     return (
-        <div className = "waterLevel" >Water Level</div>
+        <div className = "waterLevel" >Water Level
+        <button onClick={props.onClickIncrement}>+</button>
+        <button onClick={props.onClickDecrement}>-</button>
+        <h2>{props.value}%</h2>
+        </div>
     );
 }
 
