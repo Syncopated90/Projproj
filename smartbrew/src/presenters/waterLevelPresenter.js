f633673b85c52ccecdfe75d8656d65a65267666e
Serdar-b <serdar-bulun@outlook.com>
2023-05-03 14:55:29 +0200

    Added notification sounds and a data presenter and view

--- smartbrew/src/presenters/waterLevelPresenter.js
+++ smartbrew/src/presenters/waterLevelPresenter.js
@@ -1,13 +1,25 @@
 import WaterLevelView from "../views/waterLevelView";
 import React, {useState, useEffect} from 'react'
 import {writeWaterLevel, readWaterLevel} from '../firebaseModel';
+import sound from '../sounds/sound.mp3'
 
 export default function WaterLevel(){
     const [waterLevelState, setWaterLevelState] = useState(0);
+    const [hasMounted, setHasMounted] = useState(false);
+    const audio = new Audio(sound)
+    audio.volume = 0.1
 
     useEffect(() => {
         readWaterLevel("fredrik", setWaterLevelState);
     }, [])
+
+    useEffect(() => {
+        if (hasMounted && waterLevelState === 0) {
+            console.log("hello, " + "mounted?: " + hasMounted + ", waterlevel?: " + waterLevelState )
+            audio.play()
+        }
+        setHasMounted(true)
+    }, [waterLevelState])
 
     const clickedOnIncrementHandler = () =>{
         const newWaterLevel = waterLevelState + 1
