ed447920472c45e0680dc0b85ef1de7acd2b3f24
Gustav Alvestav <alvestav@kth.se>
2023-05-09 15:00:32 +0200

    Revert "Latest piprogram"
    This reverts commit ab5ac5ec6bf083892056e78a811362e4d3e4de76.

--- piprogram/piprogram.py
+++ piprogram/piprogram.py
@@ -3,7 +3,6 @@ import time
 import pyrebase
 import RPi.GPIO as GPIO
 import threading
-import queue
 from hcsr04sensor import sensor
 
 config = {
@@ -14,150 +13,100 @@ config = {
   "storageBucket": "smartbrew-1337.appspot.com"
 }
 
-
-# get a reference to the firebase app
 firebase = pyrebase.initialize_app(config)
-print("firebase reference acquired, app initialized")
-
-# get a reference to the database
 db = firebase.database()
 print("database reference acquired")
 
-# configure GPIO
-relay_pin = 14
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(relay_pin, GPIO.OUT, initial=GPIO.LOW)
 
-trig_pin = 27
+
 echo_pin = 6
+trig_pin = 27
+sample_size = 22
 hcsr04 = sensor.Measurement(trig_pin, echo_pin)
+stop_event = threading.Event()
 
 # function executes when the stream object sees a change in the database
 def stream_handler(message):
     """Handles the data read from the firebase database when a change occurs."""
     print(message)
-    if message["event"] == "patch":
-        try:
-            if message["data"]["brewingstatus"]:
-                if db.child("users").child("fredrik").child("waterLevel").get().val() > 0:
-                    print("turn on")
-                    GPIO.output(relay_pin, GPIO.HIGH)
-                else:
-                    print("refill water")
-                    db.child("users").child("fredrik").update({"brewingstatus": False})
-            
-            elif not message["data"]["brewingstatus"]:
-                GPIO.output(relay_pin, GPIO.LOW)
-                print("statusflag = false")
-        except Exception as e:
-            print(e)
+    print(threading.active_count())
+    if message["data"]:
+        if hcsr04.raw_distance(18) < 8:
+                water_thread = threading.Thread(target=water_level_handler, args=[])
+                water_thread.start()
+                GPIO.output(14, GPIO.HIGH)
+                print("Pin 14: High")
 
+        else:
+            print("not enough water")
+    else:
+        if threading.active_count() > 1:
+            stop_event.set()
+            water_thread.join()
+            stop_event.clear()
+        GPIO.output(14, GPIO.LOW)
+        print("Pin 14: Low")
+        
 # function handles reading of HC-SR04 sensor and updating database value
 # parameter: reference to database
-def water_level_handler(water_event):
+def water_level_handler():
     """Reads HC-SR04 sensor and sends the read data to firebase database."""
-    depth = hcsr04.raw_distance()
-
-    print("water thread started, depth: {}".format(round(depth, 1)))
-    while not water_event.is_set():
+    while not stop_event.is_set():
         try:
-            distance = hcsr04.raw_distance()
+            distance = hcsr04.raw_distance(sample_size)
+            distance = round(distance, 1)
+            print("{}".format(distance)) 
+            water_level = 9.5 - distance
+            water_level_percentage = round((water_level / 9.5) * 100, 0)
+            if water_level_percentage < 0:
+                water_level_percentage = 0 
+            db.child("users").child("fredrik").update({"waterLevel": water_level_percentage})
+            if distance > 9.5:
+                GPIO.output(14, GPIO.LOW)   
+                print("pin 14: Low") 
+                db.child("users").child("fredrik").update({"brewingstatus": False})
+                # stop thread here
+                stop_event.set()
+                break
         except Exception as e:
             print(e)
-            distance = 0
-            while distance == 0:
-                distance = hcsr04.raw_distance()
-
-        distance = round(distance, 1) 
-        print("{}".format(distance))
-        water_level_percentage = get_water_level_percentage(distance, depth)
-        db.child("users").child("fredrik").update({"waterLevel": water_level_percentage})
-        if water_level_percentage <= 0:
-            db.child("users").child("fredrik").update({"brewingstatus": False})
-            print("not enough water, stopped")
-        time.sleep(4)
-
-def get_water_level_percentage(distance, depth=10):
-    """Returns a percentage of the measured distance """
-    water_level = depth - distance
-    water_level_percentage = round((water_level / depth) * 100, 0)
-    if water_level_percentage < 0:
-        water_level_percentage = 0
-    return water_level_percentage
-
-
-#def main_handler(stop_event):
-#    """"""
-#    print("main handler started")
-#    gpio_state = False
-#
-#    while True:
-#        while statusflag:
-#            print("statusflag true")
-#            if stop_event.is_set():
-#                print("stop event set")
-#                break
-#            # on state
-#            if not gpio_state:
-#                print("gpio turn on")
-#                gpio_state = True
-#                GPIO.output(relay_pin, GPIO.HIGH)
-#        while not statusflag:
-#            print("statusflag false")
-#            if stop_event.is_set():
-#                print("stop event set")
-#                break
-#            # off state
-#            if gpio_state:
-#                print("gpio turn off")
-#                gpio_state = False
-#                GPIO.output(relay_pin, GPIO.LOW)
-
-
+       
+#water_thread = threading.Thread(target=water_level_handler, args=[])         
 def main():
     """Main function, program starts and runs here."""
-    
-    
-    
-    #global water_level_q = queue.Queue()
 
-    # stop_event is a flag for when the program should shut off
-    #main_event = threading.Event()
-    #main_thread = threading.Thread(target=main_handler, args=[main_event])
-    #main_thread.start()
+    # get a reference to the firebase app
+    print("firebase reference acquired, app initialized")
+    
+    print("water thread started")
 
-    water_event = threading.Event() 
-    water_thread = threading.Thread(target=water_level_handler, args=[water_event])
-    water_thread.start()
-
-    data_stream = db.child("users").child("fredrik").stream(stream_handler)
+    brewingstatus_stream = db.child("users").child("fredrik").child("brewingstatus").stream(stream_handler)
     # listen for value changes
     print("stream opened, listening for database changes")
 
     while True:
         try:
             if input("write 'exit' to stop program or press Ctrl+C\n") == "exit":
-                data_stream.close()
+                brewingstatus_stream.close()
                 print("stream closed")
-                water_event.set()
-                #main_event.set()
-                water_thread.join()
-                #main_thread.join()
-                print("water thread stopped")
+                stop_event.set()
+                #water_thread.join()
+                #print("water thread stopped")
                 GPIO.cleanup()
                 print("GPIO cleaned up")
                 sys.exit(130)
         except KeyboardInterrupt:
-            data_stream.close()
+            brewingstatus_stream.close()
             print("stream closed")
-            water_event.set()
-            #main_event.set()
-            water_thread.join()
-            #main_thread.join()
-            print("water thread stopped")
+            stop_event.set()
+            #water_thread.join()
+            #print("water thread stopped")
             GPIO.cleanup()
             print("GPIO cleaned up")
             sys.exit(130)
 
 if __name__ == "__main__":
+    # configure GPIO
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(14, GPIO.OUT, initial=GPIO.LOW)
     main()
