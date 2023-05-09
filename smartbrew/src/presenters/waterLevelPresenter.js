10e0392bc9f2e0ffc57f3358972fe8de089f3c87
danielaspin <90833955+danielaspin@users.noreply.github.com>
2023-05-09 10:19:28 +0200

    removed unused code

--- smartbrew/src/presenters/waterLevelPresenter.js
+++ smartbrew/src/presenters/waterLevelPresenter.js
@@ -1,7 +1,11 @@
 import WaterLevelView from "../views/waterLevelView";
-import React, {useState, useEffect} from 'react'
-import {writeWaterLevel, readWaterLevel, readWaterLevel2} from '../firebaseModel';
-import sound from '../sounds/sound.mp3'
+import React, { useState, useEffect } from "react";
+import {
+  writeWaterLevel,
+  readWaterLevel,
+  readWaterLevel2,
+} from "../firebaseModel";
+import sound from "../sounds/sound.mp3";
 
 export default function WaterLevel(props){
     const [waterLevelState, setWaterLevelState] = useState(0);
@@ -28,24 +32,50 @@ export default function WaterLevel(props){
         setHasMounted(true)
     }, [waterLevelState])
 
-    const clickedOnIncrementHandler = () =>{
-        const newWaterLevel = waterLevelState + 1
-        setWaterLevelState(newWaterLevel)
-        writeWaterLevel("fredrik", newWaterLevel) 
-        props.setWaterLevel(newWaterLevel);
+  useEffect(() => {
+    readWaterLevel("fredrik", setWaterLevelState, props.setWaterLevel);
+    props.setWaterLevel(waterLevelState);
+  }, []);
+  useEffect(() => {
+    readWaterLevel2("fredrik", (value) => {
+      console.log("Water level from Firebase:", value);
+      setWaterLevelState(value);
+      props.setWaterLevel(value);
+    });
+  }, []);
+  useEffect(() => {
+    if (hasMounted && waterLevelState === 0) {
+      console.log(
+        "hello, " +
+          "mounted?: " +
+          hasMounted +
+          ", waterlevel?: " +
+          waterLevelState
+      );
+      audio.play();
     }
+    setHasMounted(true);
+  }, [waterLevelState]);
 
-    const clickedOnDecrementHandler = () =>{
-        const newWaterLevel = waterLevelState - 1 
-        setWaterLevelState(newWaterLevel)
-        writeWaterLevel("fredrik", newWaterLevel)
-        props.setWaterLevel(newWaterLevel);
-    }
+  const clickedOnIncrementHandler = () => {
+    const newWaterLevel = waterLevelState + 1;
+    setWaterLevelState(newWaterLevel);
+    writeWaterLevel("fredrik", newWaterLevel);
+    props.setWaterLevel(newWaterLevel);
+  };
 
-    return (
-    <WaterLevelView 
-    onClickIncrement = {clickedOnIncrementHandler} 
-    onClickDecrement  = {clickedOnDecrementHandler} 
-    value = {waterLevelState}/>
-    );
+  const clickedOnDecrementHandler = () => {
+    const newWaterLevel = waterLevelState - 1;
+    setWaterLevelState(newWaterLevel);
+    writeWaterLevel("fredrik", newWaterLevel);
+    props.setWaterLevel(newWaterLevel);
+  };
+
+  return (
+    <WaterLevelView
+      onClickIncrement={clickedOnIncrementHandler}
+      onClickDecrement={clickedOnDecrementHandler}
+      value={waterLevelState}
+    />
+  );
 }
--- smartbrew/src/views/dataView.js
+++ smartbrew/src/views/dataView.js
@@ -1,11 +1,12 @@
-function DataView(props){
-    return (
-        <div className = "data" >Data
-        <button onClick={props.onClickIncrement}>+</button>
-        <button onClick={props.onClickDecrement}>-</button>
-        <h2>{props.value}%</h2>
-        </div>
-    );
+function DataView(props) {
+  return (
+    <div className="data">
+      Data
+      <button onClick={props.onClickIncrement}>+</button>
+      <button onClick={props.onClickDecrement}>-</button>
+      <h2>{props.value}%</h2>
+    </div>
+  );
 }
 
-export default DataView
+export default DataView;
